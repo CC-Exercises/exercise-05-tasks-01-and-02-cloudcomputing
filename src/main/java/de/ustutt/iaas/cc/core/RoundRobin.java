6ec40df9473e0a74c8bb0c2c4a1ef5bfdc7b1f50
Daniel Vietz <vietzd@gmail.com>
2017-12-01 13:32:11 +0100

    Add Round Robin scheduling strategy

---
+++ src/main/java/de/ustutt/iaas/cc/core/RoundRobin.java
@@ -0,0 +1,22 @@
+package de.ustutt.iaas.cc.core;
+
+import javax.ws.rs.client.WebTarget;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class RoundRobin implements SchedulingStrategy {
+
+    private final AtomicInteger counter = new AtomicInteger(0);
+    private final List<WebTarget> resources;
+
+    public RoundRobin(List<WebTarget> resources) {
+        this.resources = resources;
+    }
+
+    @Override
+    public WebTarget next() {
+        int index = counter.incrementAndGet() % resources.size();
+
+        return resources.get(index);
+    }
+}
