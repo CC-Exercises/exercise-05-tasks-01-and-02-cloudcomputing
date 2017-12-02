eee582a9a9eb500a5ee060204b74a324f00abce3
Daniel Vietz <vietzd@gmail.com>
2017-12-02 11:26:16 +0100

    Implement Queue Text Processor component
    This processor sends messages to a specified request queue and
    retrieves messages from a response queue. Processing worker
    components (not part of this project) take the request messages,
    process them and put them in the response queue.

--- src/main/java/de/ustutt/iaas/cc/core/QueueTextProcessor.java
+++ src/main/java/de/ustutt/iaas/cc/core/QueueTextProcessor.java
@@ -1,7 +1,33 @@
 package de.ustutt.iaas.cc.core;
 
+import java.util.UUID;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.MessageListener;
+import javax.jms.Queue;
+import javax.jms.QueueConnection;
+import javax.jms.QueueConnectionFactory;
+import javax.jms.QueueReceiver;
+import javax.jms.QueueSender;
+import javax.jms.QueueSession;
+import javax.jms.Session;
+import javax.jms.TextMessage;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import com.amazon.sqs.javamessaging.SQSConnectionFactory;
+import com.amazonaws.auth.PropertiesFileCredentialsProvider;
+import com.amazonaws.regions.Region;
+import com.amazonaws.regions.Regions;
+import com.google.common.base.Strings;
 
 import de.ustutt.iaas.cc.TextProcessorConfiguration;
 
@@ -15,30 +41,122 @@ import de.ustutt.iaas.cc.TextProcessorConfiguration;
  * request queue and write to the response queue.
  * <p>
  * This implementation supports ActiveMQ as well as AWS SQS.
- * 
- * @author hauptfn
  *
+ * @author hauptfn
  */
 public class QueueTextProcessor implements ITextProcessor {
 
-	private final static Logger logger = LoggerFactory.getLogger(QueueTextProcessor.class);
+    private final static Logger logger = LoggerFactory.getLogger(QueueTextProcessor.class);
 
-	public QueueTextProcessor(TextProcessorConfiguration conf) {
-		super();
-		logger.debug("Initializing QueueTextProcessor.");
-		
-		// TODO initialize JMS stuff, connect to queues, ...
-		
-	}
+    private QueueSession session;
+    private QueueSender sender;
+    private static ConcurrentMap<String, CompletableFuture<String>> openRequests = new ConcurrentHashMap<>();
+    private static final String MESSAGE_ID = "id";
 
-	@Override
-	public String process(String text) {
-		// text processing placeholder, to be replaced by your solution :-)
-		String processedText = "[processed by incomplete QueueTextProcessor] - " + text;
 
-		// TODO send text to request queue, receive processed text from response queue, return
+    public QueueTextProcessor(TextProcessorConfiguration conf) {
+        super();
+        try {
+            QueueConnection connection = setUpConnection();
 
-		return processedText;
-	}
+            session = connection.createQueueSession(false, Session.AUTO_ACKNOWLEDGE);
+
+            Queue requestQueue = session.createQueue(conf.requestQueueName);
+            Queue responseQueue = session.createQueue(conf.responseQueueName);
+
+            sender = session.createSender(requestQueue);
+            QueueReceiver receiver = session.createReceiver(responseQueue);
+            receiver.setMessageListener(new ML());
+
+            connection.start();
+        } catch (JMSException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private QueueConnection setUpConnection() throws JMSException {
+        QueueConnectionFactory conFactory = SQSConnectionFactory.builder()
+                .withRegion(Region.getRegion(Regions.EU_WEST_1))
+                .withAWSCredentialsProvider(new PropertiesFileCredentialsProvider("aws.properties"))
+                .build();
+        return conFactory.createQueueConnection();
+    }
+
+
+    @Override
+    public String process(String text) {
+        String uuid = UUID.randomUUID().toString();
+        CompletableFuture<String> completableFuture = new CompletableFuture<>();
+        openRequests.put(uuid, completableFuture);
+
+        // create and send text message
+        try {
+            Message msg = session.createTextMessage(text);
+            msg.setStringProperty(MESSAGE_ID, uuid);
+            logger.debug("Sending message {}", msg.getStringProperty(MESSAGE_ID));
+            sender.send(msg);
+        } catch (JMSException e) {
+            e.printStackTrace();
+        }
+
+        // wait for result (i.e. for completion of future) with timeout
+        try {
+            return completableFuture.get(5, TimeUnit.SECONDS);
+        } catch (InterruptedException | ExecutionException | TimeoutException e) {
+            e.printStackTrace();
+        }
+
+        // in case of any error, return original (unprocessed) text
+        return text;
+    }
+
+
+    /**
+     * A message listener that receives response messages from a queue and then
+     * completes the corresponding future.
+     *
+     * @author hauptfn
+     */
+    private static class ML implements MessageListener {
+        @Override
+
+        public void onMessage(Message message) {
+            if (message instanceof TextMessage) {
+                try {
+                    String messageId = getMessageId(message);
+                    if (messageId == null) return;
+
+                    CompletableFuture<String> completableFuture = getCorrespondingFuture(messageId);
+                    if (completableFuture == null) return;
+
+                    // complete future remove it from map
+                    completableFuture.complete(((TextMessage) message).getText());
+                    openRequests.remove(messageId);
+                } catch (JMSException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        private String getMessageId(Message message) throws JMSException {
+            String messageId = message.getStringProperty(MESSAGE_ID);
+            if (Strings.isNullOrEmpty(messageId)) {
+                logger.warn("Received message without ID for correlation.");
+                return null;
+            }
+            logger.debug("Received message {}", messageId);
+            return messageId;
+        }
+
+        private CompletableFuture<String> getCorrespondingFuture(String messageId) {
+            CompletableFuture<String> completableFuture = openRequests.get(messageId);
+            if (completableFuture == null) {
+                logger.warn("Cannot find request for response {}", messageId);
+                return null;
+            }
+            return completableFuture;
+        }
+
+    }
 
 }
